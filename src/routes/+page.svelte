f615ceb067bea1b778f8298b9d0905137ca94b90
koh <kohbanye@gmail.com>
2023-03-03 01:48:57 +0900

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<div class="container">
+	<img class="logo" src="logo.png" alt="logo" />
+</div>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-image: url("background.jpg");
+    background-size: cover;
+  }
+	.logo {
+		width: 100%;
+    max-width: 50rem;
+	}
+</style>
