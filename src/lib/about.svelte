b8e683715b0b0dcdd350be381e3581c662a530be
koh <kohbanye@gmail.com>
2023-03-15 15:54:57 +0900

    make about responsive

--- src/lib/about.svelte
+++ src/lib/about.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	import Section from '$lib/components/section.svelte'
 
-	const aboutString =
-		'2022年3月に結成。 ジャンルやスタイルに捉われない音楽性は、多方面で活動してきた細野陸帆(Vo.)、Zacky(Gt.)、ナカツa.k.aツギ(Ba.)、丸山隼矢(Key.)、田嶋悠人(Dr.)それぞれの個性が混ざり合って生み出されたOverflow独自の音楽である。'
+	const aboutString = `2022年3月に結成。\nジャンルやスタイルに捉われない音楽性は、多方面で活動してきた細野陸帆(Vo.)、Zacky(Gt.)、ナカツa.k.aツギ(Ba.)、丸山隼矢(Key.)、田嶋悠人(Dr.)それぞれの個性が混ざり合って生み出されたOverflow独自の音楽である。`
 </script>
 
 <Section title="ABOUT">
@@ -27,7 +26,22 @@
 		max-width: 60rem;
 	}
 	.image {
-		// TODO: make this responsive
 		width: 100vw;
+	}
+
+	@media screen and (min-width: 1024px) {
+		.container {
+			flex-direction: row;
+			align-items: flex-start;
+		}
+		.description {
+      white-space: pre-wrap;
+			max-width: 17.5rem;
+			padding: 0.5rem;
+			margin-right: 0.5rem;
+		}
+		.image {
+			width: 75%;
+		}
 	}
 </style>
