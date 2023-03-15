8843bdade19e0ed8f2741a33f802d31c0d2b2ad1
koh <kohbanye@gmail.com>
2023-03-15 15:36:22 +0900

    add about section

---
+++ src/lib/about.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import Section from '$lib/components/section.svelte'
+
+	const aboutString =
+		'2022年3月に結成。 ジャンルやスタイルに捉われない音楽性は、多方面で活動してきた細野陸帆(Vo.)、Zacky(Gt.)、ナカツa.k.aツギ(Ba.)、丸山隼矢(Key.)、田嶋悠人(Dr.)それぞれの個性が混ざり合って生み出されたOverflow独自の音楽である。'
+</script>
+
+<Section title="ABOUT">
+	<div class="container">
+		<div class="description">{aboutString}</div>
+		<img class="image" src="artist.jpg" alt="artist" />
+	</div>
+</Section>
+
+<style lang="scss">
+	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	.description {
+		font-size: large;
+		color: white;
+		padding: 0.5rem 2rem;
+		margin-bottom: 2rem;
+		max-width: 60rem;
+	}
+	.image {
+		// TODO: make this responsive
+		width: 100vw;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import Live from '$lib/live.svelte'
 	import Discography from '$lib/discography.svelte'
 	import Video from '$lib/video.svelte'
+	import About from '$lib/about.svelte'
 </script>
 
 <div class="container">
@@ -12,6 +13,7 @@
 	<Live />
 	<Discography />
 	<Video />
+	<About />
 </div>
 
 <style>
@@ -26,6 +28,7 @@
 			linear-gradient(to bottom, transparent 90%, rgba(0, 0, 0, 0.4)), url('background.jpg');
 		background-attachment: fixed;
 		background-size: cover;
+		overflow-x: hidden;
 		padding: 3rem 0;
 	}
 	.logo {
