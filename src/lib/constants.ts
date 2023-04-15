607320565f03b1ae31fc07b44c4fa2302555882a
koh <kohbanye@gmail.com>
2023-04-16 00:37:58 +0900

    improve head

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,12 @@
+export const headInfo = {
+	title: 'Overflow',
+	description:
+		'圧倒的な演奏力を武器に活動する5ピースバンド。その類稀な楽曲の数々は、独自のセンスで生み出された" Overflow "のオリジナリティである。',
+	backgroundImage: 'background.jpg',
+	logoImage: 'logo.png',
+	ogpImage: 'ogp.png'
+}
+
 export const aboutString = `2022年3月に結成。\nジャンルやスタイルに捉われない音楽性は、多方面で活動してきた細野陸帆(Vo.)、Zacky(Gt.)、ナカツa.k.aツギ(Ba.)、丸山隼矢(Key.)、田嶋悠人(Dr.)それぞれの個性が混ざり合って生み出されたOverflow独自の音楽である。`
 export const email = 'overflow0331@gmail.com'
 export const url = {
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,39 @@
 	import '@fontsource/fascinate-inline'
 	import '@fontsource/noto-sans-jp'
 	import '@fontsource/noto-sans-jp/700.css'
+
+	import { headInfo } from '$lib/constants'
 </script>
 
+<svelte:head>
+	<title>{headInfo.title}</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="description" content={headInfo.description} />
+	<link href={headInfo.backgroundImage} as="image" rel="preload" />
+	<style>
+		body {
+			font-family: 'Noto Sans JP', sans-serif;
+			background: linear-gradient(to left, transparent 85%, rgba(0, 0, 0, 0.4)),
+				linear-gradient(to right, transparent 85%, rgba(0, 0, 0, 0.4)),
+				linear-gradient(to top, transparent 90%, rgba(0, 0, 0, 0.4)),
+				linear-gradient(to bottom, transparent 90%, rgba(0, 0, 0, 0.4)), url('/background.jpg'),
+				#d33e37;
+			background-attachment: fixed;
+			background-size: cover;
+			overflow-x: hidden;
+		}
+	</style>
+	<link rel="icon" href={headInfo.logoImage} />
+  <meta property="og:title" content={headInfo.title} />
+  <meta property="og:description" content={headInfo.description} />
+  <meta property="og:image" content={headInfo.ogpImage} />
+  <meta property="og:site_name" content={headInfo.title} />
+  <meta property="og:type" content="website" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@overflow_oflo" />
+  <meta name="twitter:creator" content="@overflow_oflo" />
+  <meta name="twitter:title" content={headInfo.title} />
+  <meta name="twitter:description" content={headInfo.description} />
+  <meta name="twitter:image" content={headInfo.ogpImage} />
+</svelte:head>
 <slot />
